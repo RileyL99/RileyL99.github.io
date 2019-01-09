2db9f20a87f0f2f5749737919e54eed588d16b37
RileyL99 <43849864+RileyL99@users.noreply.github.com>
2019-01-09 23:40:04 +0000

    about update

--- js/music.js
+++ js/music.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
 	console.log("ready");
-	$("#one").click(function(event) {
-		event.preventDefault();
-		console.log("Default stopped");
-		var node = document.getElementById("concrete");
-		if(node.style.display == "none"){
-			node.style.display = "block";
-			document.getElementById("spot1").display = "block";
-		}
-		else {
-			node.style.display = "none";
-			document.getElementById("spot1").display = "none";
-		}
-	})
 });
+var myaudio = new Audio('assets\Foo Fighters - Everlong (Official Music Video) (320  kbps).mp3');
